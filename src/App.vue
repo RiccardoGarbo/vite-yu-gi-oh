db39dc0377fa1a894b0be6e5e960f068c95d7f3a
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-02-05 19:56:18 +0100

    add data to store.js file and import to file App

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script>
 import axios from 'axios'
+import { store } from './data/store'
+const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons'
 import AppPokémonList from './components/AppPokémonList.vue';
 export default {
-  components: { AppPokémonList }
+  components: { AppPokémonList },
+
+  created() {
+    axios.get(endpoint).then((res) => {
+      store.pokemonList = res.data.docs
+    })
+  }
+
 }
+
 </script>
 
 <template>
