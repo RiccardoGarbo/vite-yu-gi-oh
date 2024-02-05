dd3f6112a0af182e0e773cc5fbb79bef5aa144f2
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-02-05 19:17:17 +0100

    add tremplate PokémonCard

--- src/components/PokémonCard.vue
+++ src/components/PokémonCard.vue
@@ -3,5 +3,14 @@ export default {
     name: 'Pokémon Card'
 }
 </script>
-<template></template>
+<template>
+    <div class="col-2 h-50 d-flex align-items-center">
+        <div class="card p-4 text-center">
+            <img class="img-fluid rounded-circle " src="https://img.pokemondb.net/artwork/large/bulbasaur.jpg" alt="">
+            <span>1</span>
+            <h5>Bulbasaur</h5>
+            <p class="fst-italic">Erba</p>
+        </div>
+    </div>
+</template>
 <style></style>
